e0f348e8be92051d8455d4346f3daae9e12e8d75
Richman Tan <rtan654@aucklanduni.ac.nz>
2025-12-01 13:31:07 +1300

    Sql injection

--- server/reset.js
+++ server/reset.js
@@ -4,3 +4,15 @@ const db = initDb();
 db.exec("DELETE FROM entries;");
 seedDb(db);
 console.log("Database reset with fresh sample data.");
+
+
+const mysql = require("mysql2");
+
+// CodeQL will flag this for SQL injection
+function getUser(username) {
+  const query = "SELECT * FROM users WHERE username = '" + username + "'";
+  return query;
+}
+
+getUser("admin");
+
